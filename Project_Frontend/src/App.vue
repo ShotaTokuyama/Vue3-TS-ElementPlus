2f441ab812596d1cbc9e60e3d1311b7687248be4
tokuyama_shota <tokush1031@gmail.com>
2024-05-19 00:42:14 +0900

    first-commit

---
+++ Project_Frontend/src/App.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="common-layout">
+    <el-container>
+      <el-header>
+        <div class="header-title">Vue3 TypeScript ElementPlus</div>
+        <div class="header-btn">
+          <el-button type="primary" plain>Primary</el-button>
+          <el-button type="primary" plain>Primary</el-button>
+        </div>
+      </el-header>
+      <el-container id="container">
+        <el-aside>
+          <el-scrollbar>
+            <el-menu>
+              <el-sub-menu index="1">
+                <template #title>
+                  <el-icon><message /></el-icon>Navigator One
+                </template>
+                <el-menu-item-group>
+                  <template #title>Group 1</template>
+                  <el-menu-item index="1-1">Option 1</el-menu-item>
+                  <el-menu-item index="1-2">Option 2</el-menu-item>
+                </el-menu-item-group>
+                <el-menu-item-group title="Group 2">
+                  <el-menu-item index="1-3">Option 3</el-menu-item>
+                </el-menu-item-group>
+                <el-sub-menu index="1-4">
+                  <template #title>Option4</template>
+                  <el-menu-item index="1-4-1">Option 4-1</el-menu-item>
+                </el-sub-menu>
+              </el-sub-menu>
+              <el-sub-menu index="2">
+                <template #title>
+                  <el-icon><Service /></el-icon>Navigator Two
+                </template>
+                <el-menu-item-group>
+                  <template #title>Group 1</template>
+                  <el-menu-item index="2-1">Option 1</el-menu-item>
+                  <el-menu-item index="2-2">Option 2</el-menu-item>
+                </el-menu-item-group>
+                <el-menu-item-group title="Group 2">
+                  <el-menu-item index="2-3">Option 3</el-menu-item>
+                </el-menu-item-group>
+                <el-sub-menu index="2-4">
+                  <template #title>Option 4</template>
+                  <el-menu-item index="2-4-1">Option 4-1</el-menu-item>
+                </el-sub-menu>
+              </el-sub-menu>
+              <el-sub-menu index="3">
+                <template #title>
+                  <el-icon><setting /></el-icon>Navigator Three
+                </template>
+                <el-menu-item-group>
+                  <template #title>Group 1</template>
+                  <el-menu-item index="3-1">Option 1</el-menu-item>
+                  <el-menu-item index="3-2">Option 2</el-menu-item>
+                </el-menu-item-group>
+                <el-menu-item-group title="Group 2">
+                  <el-menu-item index="3-3">Option 3</el-menu-item>
+                </el-menu-item-group>
+                <el-sub-menu index="3-4">
+                  <template #title>Option 4</template>
+                  <el-menu-item index="3-4-1">Option 4-1</el-menu-item>
+                </el-sub-menu>
+              </el-sub-menu>
+            </el-menu>
+          </el-scrollbar>
+        </el-aside>
+        <el-main>
+          <el-card></el-card>
+        </el-main>
+      </el-container>
+    </el-container>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
